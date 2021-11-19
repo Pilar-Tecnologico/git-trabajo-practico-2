22b5a4c60d0b3ea347f6c450494e2daafde878f2
Nicolas Marinucci <nmlede1988@gmail.com>
2021-11-19 19:06:58 -0300

    doc: added jsdoc to functions

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const movies = require('./movies.json');
 
-/**
- * Complete with a brief definition of what this function should do
+/*
+ * This function must go through the array, evaluate the position of each element in the array, 
+ * and if the position is less than 10, return the element in that position.  
  * @returns {Array}
  */
-function getFirstTenMovies(){
-    //complete with your code
+function getFirstTenMovies() {
+  //complete with your code
 }
 
-/**
- * Complete with a brief definition of what this function should do
+/*
+ * This function must go through the array, evaluate if the gender of each element 
+ * is equal to the gender of the condition, and return the element that meets that condition. 
  * @param  {String} genre
  * @returns {Array}
  */
- function getMoviesWithGenre(genre){
-    //complete with your code
+function getMoviesWithGenre(genre) {
+  //complete with your code
 }
 
-function main(){
-    const firstTenMovies = getFirstTenMovies();
-    const dramaMovies = getMoviesWithGenre('Drama');
-    console.log(`Only first ten movies:\n${firstTenMovies}\n`);
-    console.log(`Only Drama movies:\n${dramaMovies}\n`);
+function main() {
+  const firstTenMovies = getFirstTenMovies();
+  const dramaMovies = getMoviesWithGenre('Drama');
+  console.log(`Only first ten movies:\n${firstTenMovies}\n`);
+  console.log(`Only Drama movies:\n${dramaMovies}\n`);
 }
 
 main();
