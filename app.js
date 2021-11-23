be006df531cd52296aea00eb90fecded5a41766d
mariaines20397 <69004580+mariaines20397@users.noreply.github.com>
2021-11-23 17:01:43 -0300

    doc: added jsdoc to functions and first exercise

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const movies = require('./movies.json');
 
 /**
- * Complete with a brief definition of what this function should do
+ * Cycle through the array. Push the first ten elements into another array
  * @returns {Array}
  */
 function getFirstTenMovies(){
     //complete with your code
+    const element=[]
+    for (let i = 0; i < movies.length; i++) {
+        if (i<10) {
+            element.push(movies[i])
+
+        }
+        
+        
+    }
+    return element
 }
 
 /**
@@ -15,13 +25,16 @@ function getFirstTenMovies(){
  */
  function getMoviesWithGenre(genre){
     //complete with your code
+    
 }
 
 function main(){
     const firstTenMovies = getFirstTenMovies();
     const dramaMovies = getMoviesWithGenre('Drama');
-    console.log(`Only first ten movies:\n${firstTenMovies}\n`);
-    console.log(`Only Drama movies:\n${dramaMovies}\n`);
+    console.log(`Only first ten movies:\n `);
+    console.log(Object.values(firstTenMovies))
+    console.log(`Only Drama movies:\n`);
+    //console.log(Object.values(dramaMovies))
 }
 
 main();
