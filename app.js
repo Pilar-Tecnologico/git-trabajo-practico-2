ba2f25994d105d06b84c93097f302513f62ccda4
Ezequiel Starecinch <echestare@gmail.com>
2021-11-23 00:16:27 -0300

    First function working

--- app.js
+++ app.js
@@ -5,7 +5,24 @@ const movies = require('./movies.json');
  * @returns {Array}
  */
 function getFirstTenMovies(){
-    //complete with your code
+    
+    let first_ten_movies = [];
+
+    for (let i=0 ; i<10 ; i++) {
+
+        first_ten_movies[i] = JSON.stringify(movies[i]) + '\n_______________________\n'; //Copying json movies to the array
+        first_ten_movies[i] = first_ten_movies[i].replace(/,"/g,'\n');  //replacing ," by a linefeed
+                                                                        //if I replace just the comma (/,/g), each comma in description will be replaced too.
+        first_ten_movies[i] = first_ten_movies[i].replace(/"/g,' ');  //replacing " by a space 
+        first_ten_movies[i] = first_ten_movies[i].replace(/[{}]/g,'');  //erasing { and }
+
+        // first_ten_movies.push(`Movie ${i+1}:\n${JSON.stringify(movies[i])}\n`);  //copying classmates.
+
+    }
+    // console.log(Array.isArray(first_ten_movies))  //Is this object an array?
+
+    return first_ten_movies;
+
 }
 
 /**
@@ -21,7 +38,8 @@ function main(){
     const firstTenMovies = getFirstTenMovies();
     const dramaMovies = getMoviesWithGenre('Drama');
     console.log(`Only first ten movies:\n${firstTenMovies}\n`);
-    console.log(`Only Drama movies:\n${dramaMovies}\n`);
+    console.log(`Only Drama movies:\n${dramaMovies}\n`);  
+
 }
 
 main();
