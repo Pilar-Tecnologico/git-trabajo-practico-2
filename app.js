e31f67bfee9e097a1a06f8fdc4a5cb2927d5f9ed
Matias Samuel Bustamante <samuelbustamante999@gmail.com>
2021-11-23 18:42:32 -0300

    Adding function to get movies by genre

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ function getFirstTenMovies(){
  * @returns {Array}
  */
  function getMoviesWithGenre(genre){
-    //complete with your code
+    return movies.filter(movie => movie.genre.includes(genre))
 }
 
 function main(){
     const firstTenMovies = getFirstTenMovies();
     const dramaMovies = getMoviesWithGenre('Drama');
-    console.log(`Only first ten movies:${JSON.stringify(firstTenMovies)}`);
-    console.log(`Only Drama movies:\n${dramaMovies}\n`);
+    console.log(`Only first ten movies:\n${JSON.stringify(firstTenMovies)}\n`);
+    console.log(`Only Drama movies:\n${JSON.stringify(dramaMovies)}\n`);
 }
 
 main();
