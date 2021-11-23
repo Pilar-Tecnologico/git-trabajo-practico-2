f8b12108e04d7d9db859e3465d331009665bb4ec
Rodrigo <rodrigo.oscar.alarcon@gmail.com>
2021-11-23 14:08:44 -0300

    First exercise solved

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const movies = require('./movies.json');
  * @returns {Array}
  */
 function getFirstTenMovies(){
-    //complete with your code
+    let m = movies;
+    let firtsTen = m.slice(0,10);
+    return firtsTen;
 }
 
 /**
@@ -21,7 +23,8 @@ function getFirstTenMovies(){
 function main(){
     const firstTenMovies = getFirstTenMovies();
     const dramaMovies = getMoviesWithGenre('Drama');
-    console.log(`Only first ten movies:\n${firstTenMovies}\n`);
+    console.log(`Only first ten movies:\n`);
+    console.log(firstTenMovies);
     console.log(`Only Drama movies:\n${dramaMovies}\n`);
 }
 
