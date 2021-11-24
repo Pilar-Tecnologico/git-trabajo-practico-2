fdc4cebe860ab93888ddd4a680668410c0aae0ed
Rafael Moyano <rafael.moyano@windriver.com>
2021-11-24 17:33:21 -0300

    doc: added jsdoc to functions.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const movies = require('./movies.json');
 
 /**
- * Complete with a brief definition of what this function should do
+ * Get the first ten movies of the list.
  * @returns {Array}
  */
 function getFirstTenMovies(){
@@ -9,7 +9,7 @@ function getFirstTenMovies(){
 }
 
 /**
- * Complete with a brief definition of what this function should do
+ * Get all the movies filter by genre name.
  * @param  {String} genre
  * @returns {Array}
  */
