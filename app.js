6f7048c04a35250d46e2baf6e7e2e88043c8fcd3
Rafael Moyano <rafael.moyano@windriver.com>
2021-11-24 17:36:28 -0300

    Exercise 1 completed.

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const movies = require('./movies.json');
  * @returns {Array}
  */
 function getFirstTenMovies(){
-    //complete with your code
+    const firstTenMovies = movies.slice(0, 10);
+    return firstTenMovies;
 }
 
 /**
@@ -20,7 +21,7 @@ function getFirstTenMovies(){
 function main(){
     const firstTenMovies = getFirstTenMovies();
     const dramaMovies = getMoviesWithGenre('Drama');
-    console.log(`Only first ten movies:\n${firstTenMovies}\n`);
+    console.log(`Only first ten movies:\n`+ JSON.stringify(firstTenMovies, null, 2)+ `\n`);
     console.log(`Only Drama movies:\n${dramaMovies}\n`);
 }
 
