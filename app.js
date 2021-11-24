47cd9c8c0983af39c59ecc4ebf2398b57a916477
Juan Robledo <juannrob@gmail.com>
2021-11-24 19:18:21 -0300

    added getFirstTenMovies function & jsdoc fix

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const movies = require('./movies.json');
 
 /**
- * Get the first ten movies of the array
+ * Get the first ten movies.
  * @returns {Array}
  */
 function getFirstTenMovies(){
-    //complete with your code
+    const first10Movies = movies.slice(0, 10);
+    return first10Movies;
 }
 
 /**
- * Get every movie of a given genre
+ * Get every movie of a given genre.
  * @param  {String} genre
  * @returns {Array}
  */
@@ -20,8 +21,8 @@ function getFirstTenMovies(){
 function main(){
     const firstTenMovies = getFirstTenMovies();
     const dramaMovies = getMoviesWithGenre('Drama');
-    console.log(`Only first ten movies:\n${firstTenMovies}\n`);
-    console.log(`Only Drama movies:\n${dramaMovies}\n`);
+    console.log("Only first ten movies:\n", firstTenMovies, "\n");
+    console.log("Only Drama movies:\n", dramaMovies, "\n");
 }
 
 main();
