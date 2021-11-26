9c03602a1659fc6b60386d382ad17cfdb1fe5e88
ejgallegos <ejgallegos@gmail.com>
2021-11-26 17:11:49 -0300

    doc: added jsdoc to functions

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const movies = require('./movies.json');
 
 /**
- * Complete with a brief definition of what this function should do
+ * Show the first ten movies.
  * @returns {Array}
  */
-function getFirstTenMovies(){
+function getFirstTenMovies() {
     //complete with your code
 }
 
 /**
- * Complete with a brief definition of what this function should do
+ * Show movies by genre.
  * @param  {String} genre
  * @returns {Array}
  */
- function getMoviesWithGenre(genre){
+function getMoviesWithGenre(genre) {
     //complete with your code
 }
 
-function main(){
+function main() {
     const firstTenMovies = getFirstTenMovies();
     const dramaMovies = getMoviesWithGenre('Drama');
     console.log(`Only first ten movies:\n${firstTenMovies}\n`);
