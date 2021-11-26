03d178c229e37328ee6d83257321d9f6a3ee88f3
Santi2104 <santif458@gmail.com>
2021-11-26 14:13:47 -0300

    doc: added jsdoc to functions

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const movies = require('./movies.json');
 
 /**
- * Complete with a brief definition of what this function should do
+ * Gets the first ten movies fron the movie array
  * @returns {Array}
  */
 function getFirstTenMovies(){
@@ -9,7 +9,7 @@ function getFirstTenMovies(){
 }
 
 /**
- * Complete with a brief definition of what this function should do
+ * Gets the movies givin a gender by parameter
  * @param  {String} genre
  * @returns {Array}
  */
