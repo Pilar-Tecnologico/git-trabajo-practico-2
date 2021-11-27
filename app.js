bbdff3a3d846fea4229e7cb2148fef3985b2b637
matias-rivera <juanmatiasrivera@outlook.com>
2021-11-26 21:27:07 -0300

    doc: added jsdoc to functions

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const movies = require('./movies.json');
 
 /**
- * Complete with a brief definition of what this function should do
+ * Returns an array which contains the first ten movies
  * @returns {Array}
  */
 function getFirstTenMovies(){
@@ -9,7 +9,7 @@ function getFirstTenMovies(){
 }
 
 /**
- * Complete with a brief definition of what this function should do
+ * Returns an array which contains movies filtered by genre
  * @param  {String} genre
  * @returns {Array}
  */
