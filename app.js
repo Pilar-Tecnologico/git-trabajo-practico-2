69e2995dcf89a4be4b56eb3f533e7e50f3f3a469
frandzo <frandzo@users.noreply.github.com>
2021-11-26 22:39:31 -0300

    Ejercicio1
    doc: added jsdoc to functions.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const movies = require('./movies.json');
+const movies = require("./movies.json");
 
 /**
  * Complete with a brief definition of what this function should do
  * @returns {Array}
  */
-function getFirstTenMovies(){
-    //complete with your code
+function getFirstTenMovies() {
+  let aux = [];
+  for (let i = 0; i < 10; i++) {
+    aux.push(movies[i].title);
+  }
+  return aux;
 }
 
 /**
@@ -13,15 +17,15 @@ function getFirstTenMovies(){
  * @param  {String} genre
  * @returns {Array}
  */
- function getMoviesWithGenre(genre){
-    //complete with your code
+function getMoviesWithGenre(genre) {
+  
 }
 
-function main(){
-    const firstTenMovies = getFirstTenMovies();
-    const dramaMovies = getMoviesWithGenre('Drama');
-    console.log(`Only first ten movies:\n${firstTenMovies}\n`);
-    console.log(`Only Drama movies:\n${dramaMovies}\n`);
+function main() {
+  const firstTenMovies = getFirstTenMovies();
+  const dramaMovies = getMoviesWithGenre("Drama");
+  console.log(`Only first ten movies:\n${firstTenMovies}\n`);
+  console.log(`Only Drama movies:\n${dramaMovies}\n`);
 }
 
 main();
