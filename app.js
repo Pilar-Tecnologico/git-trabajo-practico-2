66a70c37b1319400bd6f54293f8c5ba705d4671a
Matias N Zamora <nikoozamora93@gmail.com>
2021-11-26 22:07:01 -0300

    solving exercise number two

--- app.js
+++ app.js
@@ -14,15 +14,14 @@ function getFirstTenMovies(){
  * @returns {Array}
  */
  function getMoviesWithGenre(genre){
-    //complete with your code
+    return movies.filter((gen)=> gen.genre == genre);
 }
 
 function main(){
     const firstTenMovies = getFirstTenMovies();
     const dramaMovies = getMoviesWithGenre('Drama');
-    console.log(`Only first ten movies:\n${firstTenMovies}\n`);
-    console.log(`Only Drama movies:\n${dramaMovies}\n`);
+    console.log("Only first ten movies:\n",firstTenMovies,"\n");
+    console.log("Only Drama movies:\n",dramaMovies,"\n");
 }
-
 
 main();
