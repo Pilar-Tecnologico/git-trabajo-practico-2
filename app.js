0c8bad9546314475129260f90a1b3605471d2474
frandzo <frandzo@users.noreply.github.com>
2021-11-26 22:42:37 -0300

    Ejercicio2
    doc: added jsdoc to functions

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const movies = require("./movies.json");
 /**
  * Complete with a brief definition of what this function should do
  * @returns {Array}
+ * this function returns the first ten movies found in the .json "movies"
  */
 function getFirstTenMovies() {
   let aux = [];
@@ -16,9 +17,14 @@ function getFirstTenMovies() {
  * Complete with a brief definition of what this function should do
  * @param  {String} genre
  * @returns {Array}
+ * this function returns all the movies filtering by a genre, which is a string I give as a paramater
  */
 function getMoviesWithGenre(genre) {
-  
+    let aux = [];
+    for (let i = 0; i < 10; i++) {
+    if (movies[i].genre.includes(genre)) aux.push(movies[i].title);
+    }
+    return aux;
 }
 
 function main() {
